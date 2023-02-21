495951e11b26ac108aac4adca7d3e09744a7b9f3
AssoDePicche <samuelprado730@gmail.com>
2023-02-21 14:12:34 -0300

    feat: add board class

---
+++ src/com/assodepicche/minesweeper/model/Board.java
@@ -0,0 +1,91 @@
+package com.assodepicche.minesweeper.model;
+
+import java.util.function.Consumer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.assodepicche.minesweeper.exception.ExplosionException;
+import com.assodepicche.minesweeper.model.builder.FieldListBuilder;
+import com.assodepicche.minesweeper.model.contract.Boardlike;
+import com.assodepicche.minesweeper.model.contract.Fieldlike;
+import com.assodepicche.minesweeper.model.helper.FieldSearcher;
+import com.assodepicche.minesweeper.model.helper.FieldsLinker;
+import com.assodepicche.minesweeper.model.helper.MineScatterer;
+import com.assodepicche.minesweeper.view.helper.StringHelper;
+
+public final class Board implements Boardlike {
+  private final int rows, columns, mines;
+
+  private final List<Fieldlike> fields = new ArrayList<>();
+
+  public Board(int rows, int columns, int mines) {
+    this.rows = rows;
+
+    this.columns = columns;
+
+    this.mines = mines;
+
+    fields.addAll(FieldListBuilder.from(rows, columns));
+
+    FieldsLinker.link(fields);
+
+    MineScatterer.scatter(fields, mines);
+  }
+
+  public boolean isSolved() {
+    return fields.stream().allMatch(field -> field.isSolved());
+  }
+
+  public void open(int row, int column) {
+    Consumer<Fieldlike> open = field -> field.open();
+
+    try {
+      FieldSearcher.exec(fields, row, column, open);
+    } catch (ExplosionException exception) {
+      fields.forEach(field -> field.open());
+
+      throw exception;
+    }
+  }
+
+  public void toggleMarked(int row, int column) {
+    Consumer<Fieldlike> toggle = field -> field.toggleMarked();
+
+    FieldSearcher.exec(fields, row, column, toggle);
+  }
+
+  public void reset() {
+    fields.stream().forEach(field -> field.reset());
+
+    MineScatterer.scatter(fields, mines);
+  }
+
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+
+    builder.append(" ");
+
+    for (int column = 0; column < columns; column++) {
+      builder.append(StringHelper.append(" ", column, " "));
+    }
+
+    builder.append("\n");
+
+    int field = 0;
+
+    for (int row = 0; row < rows; row++) {
+      builder.append(row);
+
+      for (int column = 0; column < columns; column++) {
+        builder.append(StringHelper.append(" ", fields.get(field), " "));
+
+        field++;
+      }
+
+      builder.append("\n");
+    }
+
+    return builder.toString();
+  }
+}
